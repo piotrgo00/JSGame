1f41077092bc8c2ad685b6157e5a3a1a42af7594
PaweĹ <pawel.szapiel999@gmail.com>
2022-01-10 10:45:54 +0100

    setup + startowe assety

---
+++ src/config.js
@@ -0,0 +1,23 @@
+import Phaser from 'phaser';
+import {create} from "./Create";
+import {preload} from "./Preload";
+import {update} from "./Update";
+
+
+var config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 300 },
+            debug: false
+        }
+    },
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
+};
