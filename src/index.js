de2bb2b11def0e9659df778b3d192585a7dc2d04
piotrgo00 <65816799+piotrgo00@users.noreply.github.com>
2022-01-10 12:43:44 +0100

    player movement v1

--- src/index.js
+++ src/index.js
@@ -1,28 +1,80 @@
 import Phaser from 'phaser';
 
-class MyGame extends Phaser.Scene
-{
-    constructor ()
-    {
+var player, cursors;
+
+class MyGame extends Phaser.Scene {
+    constructor() {
         super();
         this.WIDTH = 800;
         this.HEIGHT = 600;
     }
 
-    preload ()
-    {
+    preload() {
         this.load.image('bg', 'src/assets/bg/hills(800x600).png');
-        this.load.image('terrain', 'src/assets/terrain(16x16).png');
-
+        //this.load.image('terrain', 'src/assets/terrain(16x16).png');
+        this.load.spritesheet('playerIdle', 'src/assets/hero/Idle (32x32).png', { frameWidth: 32, frameHeight: 32 });
+        this.load.spritesheet('playerRun', 'src/assets/hero/Run (32x32).png', { frameWidth: 32, frameHeight: 32 });
+        this.load.spritesheet('playerJump', 'src/assets/hero/Jump (32x32).png', { frameWidth: 32, frameHeight: 32 });
     }
-      
-    create ()
-    {
-        this.add.image(this.WIDTH/2, this.HEIGHT/2, 'bg');
+
+    create() {
+        let back = this.add.image(this.WIDTH / 2, this.HEIGHT / 2, 'bg');
+
+
+        //back.setOrigin(0)
+        back.setScrollFactor(0);//fixedToCamera = true;
+        this.cameras.main.setBounds(0, 0, 800, 600);
+        this.physics.world.setBounds(0, 0, 800, 600);
+
+        player = this.physics.add.sprite(400, 300, 'playerIdle');
+        player.setCollideWorldBounds(true);
+        player.setBounce(0.2);
+        this.cameras.main.startFollow(player);
+
+        this.anims.create({
+            key: 'idle',
+            frames: this.anims.generateFrameNumbers('playerIdle', { start: 0, end: 10 }),
+            frameRate: 20
+        });
+
+        this.anims.create({
+            key: 'left',
+            frames: this.anims.generateFrameNumbers('playerRun', { start: 0, end: 11 }),
+            frameRate: 20,
+            repeat: -1
+          });
+        
+          this.anims.create({
+            key: 'right',
+            frames: this.anims.generateFrameNumbers('playerRun', { start: 0, end: 11 }),
+            frameRate: 20,
+            repeat: -1
+          });
+
+
+        cursors = this.input.keyboard.createCursorKeys();
     }
 
     update(time, delta) {
         super.update(time, delta);
+        if (cursors.left.isDown) {
+            player.setVelocityX(-150);
+            player.anims.play('left', true);
+            player.flipX = true;
+        }
+        else if (cursors.right.isDown) {
+            player.setVelocityX(150);
+            player.anims.play('right', true);
+            player.flipX = false;
+        }
+        else {
+            player.setVelocityX(0);
+            player.anims.play('idle', true);
+        }
+
+        if (cursors.up.isDown && (player.body.touching.down || player.body.onFloor())) {
+            player.setVelocityY(-250);
+        }
     }
 }
 
@@ -31,6 +83,13 @@ const config = {
     parent: 'phaser-example',
     width: 800,
     height: 600,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 300 },
+            debug: false
+        }
+    },
     scene: MyGame
 };
 
